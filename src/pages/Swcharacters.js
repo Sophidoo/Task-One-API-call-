5bf15e09b1e4b25d5d683520c037d135e55593f9
Sophia Okosodo <sophieokosodo@gmail.com>
2022-12-30 19:54:03 +0100

    fetched api

--- src/pages/Swcharacters.js
+++ src/pages/Swcharacters.js
@@ -1,11 +1,32 @@
 // import Personcard from "../components/Personcard"
 import Style from "../styles/swcharacters.module.css"
 import { BiSearchAlt } from "react-icons/bi"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 const Swcharacters = () => {
 
+    useEffect(() => {
+
+        fetch('https://swapi.dev/api/people/')
+        .then((response) => response.json())
+        .then((data) => {
+            setCharData(data.results)
+            console.log(data.results)
+        })
+        .catch((err) => {console.log(err.message)})
+    }, [])
+
     const [showModal, setShowModal] = useState(false)
+    const [charData, setCharData] = useState([])
+
+    const [charIndex, setIndex] = useState()
+
+
+    const modal = (index) => {
+        setShowModal(true)
+        setIndex(index)
+
+    }
 
     return <>
         {/* <Navbar/
@@ -22,38 +43,24 @@ const Swcharacters = () => {
             </div>
 
             <section className={Style.charWrap}>
-                <div className={Style.card}>
-                    <img src="https://thumbs.dreamstime.com/b/close-up-smiling-young-black-female-fashion-model-portrait-84681420.jpg" alt="" />
-                    <h3 onClick={() => setShowModal(true)}>John Doe</h3>
-                </div>
-                <div className={Style.card}>
-                    <img src="https://thumbs.dreamstime.com/b/close-up-smiling-young-black-female-fashion-model-portrait-84681420.jpg" alt="" />
-                    <h3>John Doe</h3>
-                </div>
-                <div className={Style.card}>
-                    <img src="https://thumbs.dreamstime.com/b/close-up-smiling-young-black-female-fashion-model-portrait-84681420.jpg" alt="" />
-                    <h3>John Doe</h3>
-                </div>
-                <div className={Style.card}>
-                    <img src="https://thumbs.dreamstime.com/b/close-up-smiling-young-black-female-fashion-model-portrait-84681420.jpg" alt="" />
-                    <h3>John Doe</h3>
-                </div>
-                <div className={Style.card}>
-                    <img src="https://thumbs.dreamstime.com/b/close-up-smiling-young-black-female-fashion-model-portrait-84681420.jpg" alt="" />
-                    <h3>John Doe</h3>
-                </div>
-                <div className={Style.card}>
-                    <img src="https://thumbs.dreamstime.com/b/close-up-smiling-young-black-female-fashion-model-portrait-84681420.jpg" alt="" />
-                    <h3>John Doe</h3>
-                </div>
-                <div className={Style.card}>
-                    <img src="https://thumbs.dreamstime.com/b/close-up-smiling-young-black-female-fashion-model-portrait-84681420.jpg" alt="" />
-                    <h3>John Doe</h3>
-                </div>
-                <div className={Style.card}>
-                    <img src="https://thumbs.dreamstime.com/b/close-up-smiling-young-black-female-fashion-model-portrait-84681420.jpg" alt="" />
-                    <h3>John Doe</h3>
-                </div>
+            {
+                charData.length === 0 && 
+                <p>Loading...</p>
+            }
+            {
+                charData.length > 0 &&
+                charData.map((data, index) => {
+                    return(
+
+                        <div className={Style.card} key = {index}>
+
+                            <img src="https://thumbs.dreamstime.com/b/close-up-smiling-young-black-female-fashion-model-portrait-84681420.jpg" alt="" />
+                            <h3 onClick={() => modal(index)}>{data.name}</h3>
+                        </div>
+                    
+                )})
+            }
+                
             </section>
         </div>
 
@@ -61,19 +68,25 @@ const Swcharacters = () => {
             <p>Made With Love❤️. Sophia</p>
         </footer>
 
+        {
+            charData.length > 0 && 
+            charData.map((data, index) => 
+                index === charIndex &&
+                <div className={showModal? Style.modal : Style.hide}>
+                    <div className={Style.modalCard}>
+                        <p className={Style.close} onClick = {() => setShowModal(false)}>x</p>
+                        <img src="https://thumbs.dreamstime.com/b/close-up-smiling-young-black-female-fashion-model-portrait-84681420.jpg" alt="" />
+                        <div className={Style.details} key = {index}>
+                            <h3>{data.name}</h3>
+                            <p><span>Height: </span>{data.height}</p>
+                            <p><span>Gender: </span>{data.gender}</p>
+                        </div>
+                    </div>
+                </div> 
 
+            )
+        }
 
-        <div className={showModal? Style.modal : Style.hide}>
-            <div className={Style.modalCard}>
-                <p className={Style.close} onClick = {() => setShowModal(false)}>x</p>
-                <img src="https://thumbs.dreamstime.com/b/close-up-smiling-young-black-female-fashion-model-portrait-84681420.jpg" alt="" />
-                <div className={Style.details}>
-                    <h3>John Doe</h3>
-                    <p><span>Height: </span>250km</p>
-                    <p><span>Gender: </span>Female</p>
-                </div>
-            </div>
-        </div>
 
     </>
 
